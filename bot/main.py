40574f8277e56ced5bafced8286067c5c43e7b10
Phi-Long Bui <50190432+phi63897@users.noreply.github.com>
2020-12-29 13:01:44 -0800

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -262,7 +262,7 @@ async def on_message(message):
                 cur_price = stock.get_price().iat[0,0]
                 info += "`{}`: {:.3f} | ${:,.2f}\n".format(ticker.upper(), shares, shares*cur_price )
                 new_total += shares*cur_price
-        users_db.update_one({'user_id':message.author.id}, {'$set': {"total_assets": new_total}})
+        users_db.update_one({'user_id':int(mention)}, {'$set': {"total_assets": new_total}})
         toEmbed.add_field(name = ":bank: Total Assets", value = "${:,.2f}".format(lookup["total_assets"]), inline=False)
         toEmbed.add_field(name = ":chart_with_upwards_trend: Shares", value = info, inline=False)
         await message.channel.send(embed=toEmbed)
